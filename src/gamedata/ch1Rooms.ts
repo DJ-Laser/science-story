327bf20d1260af57a5a75ea3c314decc036f58f4
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2024-12-19 13:22:24 -0500

    ch1 formatting/grammar

--- src/gamedata/ch1Rooms.ts
+++ src/gamedata/ch1Rooms.ts
@@ -15,7 +15,7 @@ const rooms: () => [string, Room][] = () => [
       ],
       {
         goto: "meetingRoom",
-        desc: "meet the scientist",
+        desc: "Approach the scientist",
         result: [],
       },
     ),
@@ -23,7 +23,7 @@ const rooms: () => [string, Room][] = () => [
   [
     "meetingRoom",
     mkDialougeRoom(
-      'Scientist: "Ah Hello you must be the new researcher who will be joining us today."',
+      'Scientist: "Ah hello. You must be the new researcher who will be joining us today."',
       {
         goto: "questionRoom1",
         desc: "What is this place?",
@@ -50,23 +50,27 @@ const rooms: () => [string, Room][] = () => [
     questions: [
       {
         question: "Tell me more about the Manhattan project?",
-        result:
-          'J Robert Oppenheimer: "The Manhattan project is a secret project working on developing weapons capable of ending the war in favor of the allies. Scientists from both Canada and The United Kindoms have joined us to work on this project. The Manhattan project is not the first project researching atomic weapons, as far as we can tell the Germans have gotten nowhere since they started."',
+        result: [
+          "J Robert Oppenheimer: The Manhattan project is a secret project working on developing weapons capable of ending the war in favor of the allies.",
+          "Scientists from both Canada and The United Kindoms have joined us to work on this project. The Manhattan project is not the first project researching atomic weapons, but as far as we can tell the Germans have gotten nowhere since they started.",
+        ],
       },
       {
         question: "Tell me more about the weapons being developed?",
         result:
-          'J Robert Oppenheimer: "We are developing nuclear bombs. Nuclear bombs use nuclear energy to create huge reactions of power and turn that power into an explosion of huge magnitude."',
+          "J Robert Oppenheimer: We are developing nuclear bombs. Nuclear bombs use nuclear energy to create huge reactions of power and turn that power into an explosion of huge magnitude.",
       },
       {
         question: "Tell me more about nuclear energy and how it is generated?",
-        result:
-          'J Robert Oppenheimer: "Nuclear energy is the energy generated when atoms split apart, the two ways this is generated is fusion or fission. Fusion refers to the merging of two lighter atoms into heavier atoms, and Fission is the opposite, splitting one heavier atom into two smaller atoms. This causes a chain reaction that will generate huge amounts of energy. This is because the process releases neutrons that will collide with other atoms causing the same process to occur."',
+        result: [
+          "J Robert Oppenheimer: Nuclear energy is the energy generated when atoms split apart, the two ways this is generated is fusion or fission. Fusion refers to the merging of two lighter atoms into heavier atoms, and Fission is the opposite, splitting one heavier atom into two smaller atoms.",
+          "This causes a chain reaction that will generate huge amounts of energy because the process releases neutrons that will collide with other atoms, causing the same process to occur.",
+        ],
       },
       {
         question: "Tell me about yourself?",
         result:
-          'J Robert Oppenheimer: "I direct the project here at the Los Alamos laboratory, where we are designing the bombs. The other laboratories are at hanford, washington and Oak ridge, Tennessee."',
+          "J Robert Oppenheimer: I direct the project here at the Los Alamos laboratory, where we are designing the bombs. The other laboratories are at hanford, washington and Oak ridge, Tennessee.",
       },
       {
         goto: "ch2-startingRoom",
