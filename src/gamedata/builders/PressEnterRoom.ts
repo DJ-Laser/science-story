3f3c02d9aaf4385e525134f90b832c422324bc48
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2024-12-19 13:17:51 -0500

    add press enter room

---
+++ src/gamedata/builders/PressEnterRoom.ts
@@ -0,0 +1,39 @@
+import { TerminalOutput } from "../../terminal/terminalState";
+import { Choice } from "../framework/Choice";
+import { Room } from "../framework/Room";
+
+export class PressEnterRoom implements Room {
+  static defaultPrompt = ["Press enter to continue"];
+
+  description: TerminalOutput[];
+  prompt: TerminalOutput[];
+  nextRoomId: string;
+  result: TerminalOutput[];
+
+  constructor(
+    description: TerminalOutput[],
+    nextRoomId: string,
+    result: TerminalOutput[] = [],
+    prompt: TerminalOutput[] = PressEnterRoom.defaultPrompt,
+  ) {
+    this.description = description;
+    this.nextRoomId = nextRoomId;
+    this.result = result;
+    this.prompt = prompt;
+  }
+
+  getPrompt(): TerminalOutput[] {
+    const description =
+      this.description.length === 0 ? [] : [...this.description, "\n"];
+    const prompt = this.prompt.length === 0 ? [] : [...this.prompt, "\n"];
+
+    return [...description, ...prompt];
+  }
+
+  process(input: string): Choice {
+    return {
+      result: this.result,
+      destinationRoom: this.nextRoomId,
+    };
+  }
+}
